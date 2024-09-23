018085e5d8427b25859e96b97dd4ffa9933d5de6
1harshitasingh <harshita253746@gmail.com>
2024-09-23 11:57:27 +0530

    Program

---
+++ src/Java/Program1.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Program1 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            sum = sum + (2 * i);
+        }
+        System.out.print("Sum of even numbers is: ");
+        System.out.println(sum);
+    }
+}
+
+
+
