579bcb24a1cce262edc8370bd17a19212e4cdda4
1harshitasingh <harshita253746@gmail.com>
2024-09-23 14:20:47 +0530

    Array

---
+++ src/com/company/lecture1/Arrays.java
@@ -0,0 +1,13 @@
+package com.company.lecture1;
+
+public class Arrays {
+    public static void main(String args[]){
+
+        int arr[]={1,2,3,3,4,5};
+
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
+
