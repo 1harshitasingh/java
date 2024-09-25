dd8e9017366de40244b6a1004bb036f6398465cd
1harshitasingh <harshita253746@gmail.com>
2024-09-25 12:39:32 +0530

    Practice Question

---
+++ src/com/company/lecture2/Practice2.java
@@ -0,0 +1,16 @@
+package com.company.lecture2;
+
+public class Practice2 {
+    static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+        public static void main (String[]args){
+            pattern(9);
+        }
+    }
