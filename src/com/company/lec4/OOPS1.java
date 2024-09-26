f81a56e41942984e9660eae920e41a65aedf68c1
1harshitasingh <harshita253746@gmail.com>
2024-09-26 13:37:49 +0530

    Question

---
+++ src/com/company/lec4/OOPS1.java
@@ -0,0 +1,31 @@
+package com.company.lec4;
+
+class Employee {
+        int id;
+        int salary;
+        String name;
+        public void printDetails(){
+            System.out.println("My id is " + id);
+            System.out.println("and my name is "+ name);
+        }
+
+        public int getSalary(){
+            return salary;
+        }
+    }
+
+    public class OOPS1 {
+        public static void main(String[] args) {
+            System.out.println("This is our custom class");
+            Employee E1 = new Employee();
+            Employee E2 = new Employee();
+            E1.name="Harshita";
+            E2.name="Ron";
+            E1.printDetails();
+            E2.printDetails();
+
+
+        }
+    }
+
+
