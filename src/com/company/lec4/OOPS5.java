34f827d7b60134f1a5aefcc75aba5e8ed9b50069
1harshitasingh <harshita253746@gmail.com>
2024-09-26 14:57:36 +0530

    Practice Question

---
+++ src/com/company/lec4/OOPS5.java
@@ -0,0 +1,24 @@
+package com.company.lec4;
+
+class Tommy{
+    public void hit(){
+        System.out.println("Hitting the enemy");
+    }
+    public void run(){
+        System.out.println("Running from the enemy");
+    }
+    public void fire(){
+        System.out.println("Firing on the enemy");
+    }
+}
+
+public class OOPS5 {
+    public static void main(String[] args) {
+
+        Tommy player1 = new Tommy();
+        player1.fire();
+        player1.run();
+        player1.hit();
+
+    }
+}
