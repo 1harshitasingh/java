249b55a54fac7f27fc34f9c58acded71b3b031b9
1harshitasingh <harshita253746@gmail.com>
2024-09-26 15:18:31 +0530

    Practice

---
+++ src/com/company/lec4/OOPS7.java
@@ -0,0 +1,27 @@
+package com.company.lec4;
+
+class Circle {
+    float radius;
+
+    public float area() {
+        return 3.14f * radius * radius;
+    }
+
+    public float perimeter() {
+        return 2 * 3.14f * radius;
+    }
+
+
+}
+
+
+public class OOPS7 {
+    public static void main(String[] args) {
+        Circle  c = new Circle();
+        c.radius = 4;
+
+        System.out.println(c.area());
+        System.out.println(c.perimeter());
+
+    }
+}
