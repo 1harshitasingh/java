687a2d36e06159442696cade6996cbfcf9ab402c
1harshitasingh <harshita253746@gmail.com>
2024-09-27 17:13:44 +0530

    Practice Question

--- src/com/company/lec5/Que.java
+++ src/com/company/lec5/Que.java
@@ -1,48 +1,48 @@
 package com.company.lec5;
 
 
-    class Base{
-        public int x;
+class Base {
+    public int x;
 
-        public int getX() {
-            return x;
-        }
-
-        public void setX(int x) {
-            System.out.println(" In base and setting x now");
-            this.x = x;
-        }
-
-        public void printMe(){
-            System.out.println(" constructor");
-        }
+    public int getX() {
+        return x;
     }
 
-    class Derived extends Base{
-        public int y;
-
-        public int getY() {
-            return y;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
+    public void setX(int x) {
+        System.out.println(" In base and setting x now");
+        this.x = x;
     }
 
-    public class Que {
-        public static void main(String[] args) {
-
-            Base b = new Base();
-            b.setX(4);
-            System.out.println(b.getX());
-
-
-            Derived d = new Derived();
-            d.setY(43);
-            System.out.println(d.getY());
-
-        }
+    public void printMe() {
+        System.out.println(" constructor");
     }
+}
+
+class Derived extends Base {
+    public int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+public class Que {
+    public static void main(String[] args) {
+
+        Base b = new Base();
+        b.setX(4);
+        System.out.println(b.getX());
+
+
+        Derived d = new Derived();
+        d.setY(43);
+        System.out.println(d.getY());
+
+    }
+}
 
 
