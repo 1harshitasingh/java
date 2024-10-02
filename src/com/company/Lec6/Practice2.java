5e0862d5748ad0719ae562fd61eadcbc9e566043
1harshitasingh <harshita253746@gmail.com>
2024-10-02 10:49:16 +0530

    Question

---
+++ src/com/company/Lec6/Practice2.java
@@ -0,0 +1,44 @@
+package com.company.Lec6;
+
+class Monkey{
+    void jump(){
+        System.out.println("Jumping...");
+    }
+    void bite(){
+        System.out.println("Biting...");
+    }
+}
+
+interface BasicAnimal{
+    void eat();
+    void sleep();
+}
+
+class Human extends Monkey implements BasicAnimal{
+    void speak(){
+        System.out.println("Hello sir!");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
+}
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Human h=new Human();
+        h.eat();
+        h.sleep();
+
+        Monkey m1=new Human();
+        m1.bite();
+
+
+    }
+}
