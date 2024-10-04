9a3dbecb573f7449fbd15ccf3d6833ec6aaff362
1harshitasingh <harshita253746@gmail.com>
2024-10-04 13:33:57 +0530

    Q

--- src/com/company/lec7/Eh2.java
+++ src/com/company/lec7/Eh2.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 
 public class Eh2 {
     public static void main(String[] args) {
-            int marks[]={7,56,6};
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter the value of index");
-            int ind = sc.nextInt();
-            try{
-                System.out.println("Welcome");
-                try {
-                    System.out.println(marks[ind]);
-                }
-                catch(ArrayIndexOutOfBoundsException e){
-                    System.out.println("Index does not exist");
-                    System.out.println("Exception in level 2");
-                }
+        int marks[] = {7, 56, 6};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of index");
+        int ind = sc.nextInt();
+        try {
+            System.out.println("Welcome");
+            try {
+                System.out.println(marks[ind]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Index does not exist");
+                System.out.println("Exception in level 2");
             }
-            catch(Exception e){
-                System.out.println("Exception in level 1");
-            }
+        } catch (Exception e) {
+            System.out.println("Exception in level 1");
+        }
     }
 }
