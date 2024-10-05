7f912b807ee87d01293010f18311342359a81027
1harshitasingh <harshita253746@gmail.com>
2024-10-05 20:23:24 +0530

    Question

---
+++ src/com/company/lec7/Arraylist1.java
@@ -0,0 +1,21 @@
+package com.company.lec7;
+
+import java.util.*;
+
+public class Arraylist1 {
+
+        public static void main(String[] args) {
+
+            ArrayList<Integer> l1 = new ArrayList<>();
+            l1.add(1);
+            l1.add(2);
+            l1.add(3);
+            l1.add(4);
+            l1.add(6);
+            l1.add(5,5);
+
+
+            System.out.println(l1);
+        }
+    }
+
