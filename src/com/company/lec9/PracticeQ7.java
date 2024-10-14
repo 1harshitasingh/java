84bde535d7d0f2637e19d24096a246f7a65ddfdc
1harshitasingh <harshita253746@gmail.com>
2024-10-14 10:07:49 +0530

    Practice Question

---
+++ src/com/company/lec9/PracticeQ7.java
@@ -0,0 +1,21 @@
+package com.company.lec9;
+
+import java.util.*;
+
+public class PracticeQ7 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("Red");
+        list.add("Green");
+        list.add("Orange");
+        list.add("White");
+        list.add("Black");
+        System.out.println(list);
+        for (String element : list) {
+            System.out.println(element);
+        }
+    }
+}
+
+
+
